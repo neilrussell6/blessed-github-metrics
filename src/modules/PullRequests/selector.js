758e2eef5bb03e6311e65257ca8787d743dbc0f7
Neil Russell <neilrussell6@gmail.com>
2019-09-08 16:52:20 +0200

    refactor(PullRequests): move table config to static settings and fix selector tests.

--- src/modules/PullRequests/selector.js
+++ src/modules/PullRequests/selector.js
@@ -1,24 +1,14 @@
 const R = require ('ramda')
 const { createSelector } = require ('reselect')
 
+const Settings = require('../../config/settings')
 const _R = require ('../../common/utils/ramda.utils')
-const { FORMAT_TEMPLATES } = require('./constants')
 
 const selector = createSelector ([R.identity], R.pipe (
   _R.assocSpec ({
     pullRequests: R.prop('pullRequests'),
     isFocused: R.pathEq (['app', 'selectedSectionIndex'], 0),
-    columnsConfig: R.always ([
-      { key: 'title', label: 'TITLE', width: 28, ellipsis: 'end' },
-      // { key: 'baseRefName', label: 'BASE', width: 12, ellipsis: 'end' },
-      // { key: 'headRefName', label: 'BRANCH', width: 14, ellipsis: 'end' },
-      { key: 'authorLogin', label: 'AUTHOR', width: 16, ellipsis: 'end' },
-      { key: 'state', label: 'STATE', width: 10 },
-      { key: 'createdAt', label: 'CREATED', width: 16, ellipsis: 'start', format: 'date', formatTemplate: FORMAT_TEMPLATES.DATE },
-      { key: 'publishedAt', label: 'PUBLISHED', width: 16, ellipsis: 'start', format: 'date', formatTemplate: FORMAT_TEMPLATES.DATE },
-      { key: 'updatedAt', label: 'UPDATED', width: 16, ellipsis: 'start', format: 'date', formatTemplate: FORMAT_TEMPLATES.DATE },
-      { key: 'mergedAt', label: 'MERGED', width: 16, ellipsis: 'start', format: 'date', formatTemplate: FORMAT_TEMPLATES.DATE },
-    ]),
+    columnsConfig: R.always (R.path (['pullRequests' , 'tableColumnsConfig']) (Settings)),
   }),
 ))
 
--- src/modules/PullRequests/selector.test.js
+++ src/modules/PullRequests/selector.test.js
@@ -1,117 +1,34 @@
-// const { assert } = require ('chai')
-// const sinon = require ('sinon')
-//
-// const SUT = require ('./selector')
-//
-// describe ('modules/PullRequests/selector', () => {
-//   let sandbox = null
-//
-//   beforeEach (async () => {
-//     sandbox = await sinon.createSandbox ()
-//   })
-//
-//   afterEach (async () => {
-//     await sandbox.restore ()
-//   })
-//
-//   describe ('general', () => {
-//     it ('should return required view props as expected', () => {
-//       const state = {
-//         pullRequests: [
-//           {
-//             label: 'TETH PullRequest 1',
-//             address: '0xc94655ded2bdfe8a476ff7e654055109e2946c90',
-//             blockchain: 'TETH',
-//             balances: {
-//               TETH: '1000',
-//               TERC: '10',
-//             },
-//           },
-//           // {
-//           //   label: 'TETH PullRequest 2',
-//           //   address: '0xc94655ded2bdfe8a476ff7e654055109e2946c91',
-//           //   blockchain: 'TETH',
-//           //   balances: {
-//           //     TETH: '200',
-//           //     TERC: '2',
-//           //   },
-//           // },
-//           {
-//             label: 'ETH PullRequest 1',
-//             address: '0xc94655ded2bdfe8a476ff7e654055109e2946c92',
-//             blockchain: 'ETH',
-//             balances: {
-//               ETH: '300000',
-//               // EOS: '3000',
-//               // BNB: '300',
-//               // OMG: '30',
-//             },
-//           },
-//           // {
-//           //   label: 'TBTC PullRequest 1',
-//           //   address: '2N8pRPYeHfyMU6EZMe93kPzEP7dnd3cxFMx',
-//           //   blockchain: 'TBTC',
-//           //   balances: {
-//           //     TBTC: '400000',
-//           //   },
-//           // },
-//           // {
-//           //   label: 'BTC PullRequest 1',
-//           //   address: '2N8pRPYeHfyMU6EZMe93kPzEP7dnd3cxFMx',
-//           //   blockchain: 'BTC',
-//           //   balances: {
-//           //     BTC: '500000',
-//           //     BCH: '50000',
-//           //     LTC: '5000',
-//           //     RMG: '500',
-//           //   },
-//           // },
-//         ],
-//       }
-//
-//       SUT (state)
-//       // assert.deepEqual (SUT (state), {
-//       //   pullRequestBalanceTotals: {
-//       //     TERC: '12',
-//       //     TETH: '1200',
-//       //   },
-//       //   columnsConfig: [
-//       //     {
-//       //       key: 'label',
-//       //       label: 'NAME',
-//       //       width: 32,
-//       //     },
-//       //     {
-//       //       key: 'address',
-//       //       label: 'ADDRESS',
-//       //       width: 21,
-//       //     },
-//       //     {
-//       //       key: 'TETH',
-//       //       label: 'TETH',
-//       //       width: 12,
-//       //     },
-//       //     {
-//       //       key: 'TERC',
-//       //       label: 'TERC',
-//       //       width: 12,
-//       //     },
-//       //   ],
-//       //   rows: [
-//       //     [
-//       //       'TETH PullRequest 1',
-//       //       '0xc9...6c90',
-//       //       '1000',
-//       //       '10',
-//       //     ],
-//       //     [
-//       //       'TETH PullRequest 2',
-//       //       '0xc9...6c91',
-//       //       '200',
-//       //       '2',
-//       //     ],
-//       //   ],
-//       // })
-//     })
-//   })
-// })
+const { assert } = require ('chai')
+const parametrize = require ('js-parametrize')
+
+const SUT = require ('./selector')
+const Settings = require ('../../config/settings')
+
+describe ('modules/PullRequests/selector', () => {
+  describe ('pullRequests', () => {
+    it ('should return pullRequests directly from state as is', () => {
+      const state = {
+        pullRequests: 'STATE PULL REQUESTS',
+      }
+      assert.equal (SUT (state).pullRequests, 'STATE PULL REQUESTS')
+    })
+  })
+
+  describe ('isFocused', () => {
+    parametrize ([
+      [{ app: { selectedSectionIndex: 0 } }, true],
+      [{ app: { selectedSectionIndex: 1 } }, false],
+    ], (state, expected) => {
+      it ('should return isFocused as expected', () => {
+        assert.equal (SUT (state).isFocused, expected)
+      })
+    })
+  })
+
+  describe ('columnsConfig', () => {
+    it ('should return columnsConfig from app settings', () => {
+      const state = {}
+      assert.deepEqual (SUT (state).columnsConfig, Settings.pullRequests.tableColumnsConfig)
+    })
+  })
+})
