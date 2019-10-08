3c5519ca87ee6a1f4e5ca6ea12e5a18af17ccaf4
Neil Russell <neilrussell6@gmail.com>
2019-10-08 22:49:59 +0200

    feat(PullRequestEvents): update utils to attach responsibility events to PR event participants.

--- src/modules/PullRequestEvents/utils.js
+++ src/modules/PullRequestEvents/utils.js
@@ -4,7 +4,25 @@ const R = require ('ramda')
 // set participant is responsible
 //---------------------------------
 
-const setParticipantIsResponsible = event => participant => R.mergeDeepLeft ({ isResponsible: true }) (participant)
+const setParticipantIsResponsible = event => participant => {
+  const responsibilityEvents = R.compose (R.propOr ([]) ('responsibilityEvents')) (participant)
+  const eventDatetime = R.prop ('at') (event)
+  const _responsibilityEvents = R.ifElse (
+    // ... if no events, or latest event already has an end datetime set
+    R.anyPass ([
+      R.isEmpty,
+      R.compose (R.not, R.propSatisfies (R.isNil) ('end'), R.last),
+    ]),
+    // ... then create initial event
+    R.append ({ start: eventDatetime, end: null }),
+    // ... otherwise do nothing
+    R.identity,
+  ) (responsibilityEvents)
+  return R.mergeDeepLeft ({
+    isResponsible: true,
+    responsibilityEvents: _responsibilityEvents,
+  }) (participant)
+}
 
 module.exports.setParticipantIsResponsible = setParticipantIsResponsible
 
@@ -12,6 +30,25 @@ module.exports.setParticipantIsResponsible = setParticipantIsResponsible
 // set participant is not responsible
 //---------------------------------
 
-const setParticipantIsNotResponsible = event => participant => R.mergeDeepLeft ({ isResponsible: false }) (participant)
+const setParticipantIsNotResponsible = event => participant => {
+  const responsibilityEvents = R.compose (R.propOr ([]) ('responsibilityEvents')) (participant)
+  const lastIndex = R.compose (R.subtract (R.__, 1), R.length) (responsibilityEvents)
+  const eventDatetime = R.prop ('at') (event)
+  const _responsibilityEvents = R.ifElse (
+    // ... if no events, or latest event already has an end datetime set
+    R.anyPass ([
+      R.isEmpty,
+      R.compose (R.not, R.propSatisfies (R.isNil) ('end'), R.last),
+    ]),
+    // ... then do nothing
+    R.identity,
+    // ... otherwise set end datetime
+    R.adjust (lastIndex) (R.assoc ('end', eventDatetime)),
+  ) (responsibilityEvents)
+  return R.mergeDeepLeft ({
+    isResponsible: false,
+    responsibilityEvents: _responsibilityEvents,
+  }) (participant)
+}
 
 module.exports.setParticipantIsNotResponsible = setParticipantIsNotResponsible
--- src/modules/PullRequestEvents/utils.test.js
+++ src/modules/PullRequestEvents/utils.test.js
@@ -10,6 +10,29 @@ describe ('modules/PullRequestEvents/utils', () => {
       const result = SUT.setParticipantIsResponsible (event) (participant)
       assert.isTrue (result.isResponsible)
     })
+
+    it ('should create a new responsibility event with start time if none', () => {
+      const event = { at: 'EVENT AT'}
+      const participant = {}
+      const result = SUT.setParticipantIsResponsible (event) (participant)
+      assert.deepEqual (result.responsibilityEvents, [
+        { start: 'EVENT AT', end: null },
+      ])
+    })
+
+    it ('should create a new responsibility event with start time if previous event is complete', () => {
+      const event = { at: 'EVENT AT'}
+      const participant = {
+        responsibilityEvents: [
+          { start: 'START TIME', end: 'END TIME' },
+        ],
+      }
+      const result = SUT.setParticipantIsResponsible (event) (participant)
+      assert.deepEqual (result.responsibilityEvents, [
+        { start: 'START TIME', end: 'END TIME' },
+        { start: 'EVENT AT', end: null },
+      ])
+    })
   })
 
   describe ('setParticipantIsNotResponsible', () => {
@@ -18,6 +41,39 @@ describe ('modules/PullRequestEvents/utils', () => {
       const participant = {}
       const result = SUT.setParticipantIsNotResponsible (event) (participant)
       assert.isFalse (result.isResponsible)
+    })
+
+    it ('should not create a new responsibility event if none', () => {
+      const event = { at: 'EVENT AT'}
+      const participant = {}
+      const result = SUT.setParticipantIsNotResponsible (event) (participant)
+      assert.deepEqual (result.responsibilityEvents, [])
+    })
+
+    it ('should complete latest responsibility event setting end time', () => {
+      const event = { at: 'EVENT AT'}
+      const participant = {
+        responsibilityEvents: [
+          { start: 'START TIME', end: null },
+        ],
+      }
+      const result = SUT.setParticipantIsNotResponsible (event) (participant)
+      assert.deepEqual (result.responsibilityEvents, [
+        { start: 'START TIME', end: 'EVENT AT' },
+      ])
+    })
+
+    it ('should not complete latest responsibility event if already complete', () => {
+      const event = { at: 'EVENT AT'}
+      const participant = {
+        responsibilityEvents: [
+          { start: 'START TIME', end: 'END TIME' },
+        ],
+      }
+      const result = SUT.setParticipantIsNotResponsible (event) (participant)
+      assert.deepEqual (result.responsibilityEvents, [
+        { start: 'START TIME', end: 'END TIME' },
+      ])
     })
   })
 })
